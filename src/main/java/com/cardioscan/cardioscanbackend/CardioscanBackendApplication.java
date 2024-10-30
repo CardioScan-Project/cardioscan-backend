1dc5ba8e6675c81b43230d106a133e1640e08f5e
Edgar Andre Anco Copaja <89050053+edgar-anco@users.noreply.github.com>
2024-10-29 23:56:03 -0500

    chore: initialize Spring Boot project with PostgreSQL configuration

---
+++ src/main/java/com/cardioscan/cardioscanbackend/CardioscanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cardioscan.cardioscanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardioscanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardioscanBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardioscan/cardioscanbackend/CardioscanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardioscan.cardioscanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardioscanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
