9c15068cbce321e58c35471219f2ce6e2cad0e94
Edgar Andre Anco Copaja <89050053+edgar-anco@users.noreply.github.com>
2024-10-30 16:55:35 -0500

    feat(entity): add Doctor entity

---
+++ src/main/java/com/cardioscan/cardioscanbackend/entities/Doctor.java
@@ -0,0 +1,25 @@
+package com.cardioscan.cardioscanbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "doctors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+    @Column(name = "email", nullable = false, length = 150)
+    private String email;
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+}
